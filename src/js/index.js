92577f255f58f551cec5c7afab4b787748177810
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-08-07 14:41:38 -0400

    add functionality

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+let currentTheme = 'amber'
+const btns = document.querySelectorAll('.themes a')
+const screen = document.getElementById('screen')
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    let newTheme = event.target.id
+    screen.classList.remove(currentTheme)
+    screen.classList.add(newTheme)
+  })
+})
